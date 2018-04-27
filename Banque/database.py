4f4c34aff6f473856d0505ee23b10617f035c847
John PENDENQUE <pendenquejohn@gmail.com>
2018-04-27 18:03:59 +0200

    'Ameliorations'

--- Banque/database.py
+++ Banque/database.py
@@ -1,57 +1,54 @@
 import sqlite3 as db
 import os
 
-DATABASE_PATH = os.path.join(os.getcwd(), 'Python\\Banque\\bank_db.db')
+# DATABASE_PATH = os.path.join(os.getcwd(), 'Python\\python_codes\\Banque\\bank_db.db')
+DATABASE_PATH = 'bank_db.db'
 
 class DataBaseMixins:
     context = None
     
     def get_context(self, *args, **kwargs):
+        # Puts context
         if self.context is not None:
             kwargs.update(context=self.context)
 
+        # Puts instance of class
         if 'method' not in kwargs:
             kwargs['method'] = self
-            
+
         return kwargs
 
-print(DataBaseMixins().get_context(context=''))
+
+class DataBase(DataBaseMixins):
+        def _cursor(self):
+            try:
+                # Connect to database, 
+                _conn = db.connect(DATABASE_PATH)
+
+            except Exception as error:
+                print('There was an error. %s' % error.args )
+                raise
+            
+            with _conn:
+                self._cursor = _conn.cursor()
+                return self._cursor
 
 
-# class DataBase(DataBaseMixins):
-#     timeout = None
+class DatabaseQueries(DataBase):
+    table_name = None
 
-#     def __init__(self):
-#         if type(timeout).__name__ != 'int':
-#             raise TypeError('Value should be an integer. Got %s' % timeout)
+    def select_all(self, *args, **kwargs):
+        if 'table_name' in kwargs:
+            self.table_name = kwargs.get('table')
 
-#         # Connect to database
-#         try:
-#             _conn = db.connect(DATABASE_PATH, timeout)
+        records = super()._cursor().execute('SELECT ? FROM ?', (args, self.table_name,))
+        print(records)
+        # if 'records' not in kwargs:
+        #     kwargs['records'] = records
 
-#         except Exception as error:
-#             print('There was an error. %s' % error.args )
-#             raise
-        
-#         with _conn:
-#             self._cursor = _conn.cursor()
-#             return self._cursor
+        # else:
+        #     kwargs.update(records=records)
 
+        # return kwargs
 
-# class Queries(DataBase):
-#     table_name = None
-
-#     def select_all(self, *args, **kwargs):
-#         if 'table_name' in kwargs:
-#             self.table_name = kwargs.get('table')
-
-#         self._cursor.execute('SELECT ? FROM ?', (args, self.table_name,))
-
-    # def select(self, *args, **kwargs):
-    #     SQL = 'SE'
-
-    # def insert(self, *args, **kwargs):
-    #     pass
-
-    # def delete(self, *args, **kwargs):
-    #     pass
+print(DatabaseQueries().select_all())
