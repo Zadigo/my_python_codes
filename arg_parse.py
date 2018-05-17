2229a21407b9c5e2aa40e349d8bfa3aff6c1f5f9
John PENDENQUE <pendenquejohn@gmail.com>
2018-05-17 16:40:14 +0200

    A

---
+++ arg_parse.py
@@ -0,0 +1,22 @@
+# from argparse import ArgumentParser
+
+# def _parse_args():
+#     args = ArgumentParser(
+#         description='Create a Luhn number'
+#     )
+#     args.add_argument('number', help='Enter a 7 digit number', type=int)
+#     args.add_argument('-e', '--encrypt', help='Encrypt nuner using 5')
+#     args.parse_args()
+#     return args
+
+# def _create_nubers():
+#     pass
+
+# def Main():
+#     arg = _parse_args()
+#     if arg:
+#         print('True')
+#         print(arg._get_args())
+
+# if __name__ == '__main__':
+#     Main()
