41cfee2550a33864949f1da270c75d5a0172d4dc
TalentView <talentview@iMac-de-TalentView.local>
2018-07-17 10:47:06 +0200

    URL refactoring

--- custom_apps/accounts/urls.py
+++ custom_apps/accounts/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 from accounts import views
 
 urlpatterns = [
-    url(r'^forgot-password', views.forgot_password, name='forgot'),
-    url(r'^signup', views.signup_user, name='signup'),
-    url(r'^login', views.login_user, name='login'),
-    url(r'^logout', views.logout_user, name='logout'),
-    url(r'^profile/edit/', views.ProfileEditView.as_view(), name='profile_edit'),
-    url(r'^profile', views.ProfileView.as_view(), name='profile'),
+    url(r'^forgot-password/$', views.forgot_password, name='forgot'),
+    url(r'^signup/$', views.signup_user, name='signup'),
+    url(r'^login/$', views.login_user, name='login'),
+    url(r'^logout/$', views.logout_user, name='logout'),
+    url(r'^profile/edit/$', views.ProfileEditView.as_view(), name='profile_edit'),
+    url(r'^profile/$', views.ProfileView.as_view(), name='profile'),
     url(r'^$', views.accounts_redirection, name='accounts_redirection'),
 ]
