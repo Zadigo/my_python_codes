dcec20c3b750a72231bb178cf2b37189378f3b34
John <pendenquejohn@gmail.com>
2019-10-20 00:43:51 +0200

    Refactoring elements

--- random_string_generator.py
+++
@@ -1,7 +0,0 @@
-import random
-import string
-
-def generator(size=10, chars=''):
-    if  not chars:
-        chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
-    return ''.join(random.choice(chars) for _ in range(size))
--- scrapper.py
+++
@@ -1,6 +0,0 @@
-# from games.bank.database.manager import Model
-# from games.bank.database.fields import IntegerField
-
-# class Person(Model):
-#     name = IntegerField(12)
-
